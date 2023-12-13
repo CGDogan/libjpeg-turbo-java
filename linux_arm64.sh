0fdc8d1060638c1f56fd6ec2eb51f7d79d4bf7e7
CGDogan <126820728+CGDogan@users.noreply.github.com>
2023-12-13 11:04:03 +0000

    Update linux_arm64.sh

--- linux_arm64.sh
+++ linux_arm64.sh
@@ -1,14 +1,1 @@
-#!/bin/bash
 
-set -x
-
-cd "$(dirname "${0}")"
-export JAVA_HOME="/usr/lib/jvm/java-1.8.0-openjdk"
-export CPPFLAGS="-I${JAVA_HOME}/include -I${JAVA_HOME}/include/linux"
-cd libjpeg-turbo
-autoreconf -fiv
-mkdir build
-cd build
-../configure --with-java
-make
-ls .libs
